42dfa25caa66512fd36ac338465fee9e5b094db6
rishuriya <rdrishav2002@yahoo.com>
2022-01-20 01:37:55 +0530

    added static and detail page

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
 class Post(models.Model):
     author= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    imag=models.ImageField(upload_to="images/")
+    title = models.CharField(max_length=150)
+    subtitle=models.CharField(max_length=300)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
