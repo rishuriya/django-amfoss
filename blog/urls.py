7e75aeb5867ef22be9e97031a9d2d2d82c59c332
rishuriya <rdrishav2002@yahoo.com>
2022-01-26 23:21:00 +0530

    adding form and new frontend

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.post_list, name='post_wall'),
+    path('add_blog/',views.temp),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     ]
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
